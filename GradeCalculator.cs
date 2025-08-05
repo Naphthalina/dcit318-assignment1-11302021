fa85b529b8880888a92dd3726dcfec668c1e64b0
Naphthalina <bnassefuah@st.ug.edu.gh>
2025-08-05 12:48:45 -0400

    Made the Grade Calculator

---
+++ GradeCalculator.cs
@@ -0,0 +1,21 @@
+using System;
+
+class GradeCalculator
+{
+    static void Main()
+    {
+        Console.Write("Enter a numerical grade (0-100): ");
+        int grade = int.Parse(Console.ReadLine());
+
+        if (grade >= 90)
+            Console.WriteLine("Letter grade: A");
+        else if (grade >= 80)
+            Console.WriteLine("Letter grade: B");
+        else if (grade >= 70)
+            Console.WriteLine("Letter grade: C");
+        else if (grade >= 60)
+            Console.WriteLine("Letter grade: D");
+        else
+            Console.WriteLine("Letter grade: F");
+    }
+}
