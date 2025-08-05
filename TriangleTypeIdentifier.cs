816e249299a68a06c0f929eeecf122bc6a502869
Naphthalina <bnassefuah@st.ug.edu.gh>
2025-08-05 12:51:08 -0400

    Triangle  Id

---
+++ TriangleTypeIdentifier.cs
@@ -0,0 +1,21 @@
+using System;
+
+class TriangleTypeIdentifier
+{
+    static void Main()
+    {
+        Console.Write("Enter side 1: ");
+        int side1 = int.Parse(Console.ReadLine());
+        Console.Write("Enter side 2: ");
+        int side2 = int.Parse(Console.ReadLine());
+        Console.Write("Enter side 3: ");
+        int side3 = int.Parse(Console.ReadLine());
+
+        if (side1 == side2 && side2 == side3)
+            Console.WriteLine("Triangle type: Equilateral");
+        else if (side1 == side2 || side1 == side3 || side2 == side3)
+            Console.WriteLine("Triangle type: Isosceles");
+        else
+            Console.WriteLine("Triangle type: Scalene");
+    }
+}
